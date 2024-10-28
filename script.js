d855aa21f58eea8092540ff37bfaeb377a5f4f5c
soy jeison <jeisonhv96@gmail.com>
2024-10-28 10:58:09 -0500

    base-exponente2

---
+++ script.js
@@ -0,0 +1,5 @@
+let base = parseFloat(prompt("Ingresa la base:"));
+let exponente = parseFloat(prompt("Ingresa el exponente:"));
+let resultado = Math.pow(base, exponente);
+
+console.log("El resultado obtenido es " + resultado);
